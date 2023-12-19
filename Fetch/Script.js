aafb7bd072467e886019d97d33a1922104f354fe
Mohammad Ali Sk <mohammadalisk050@gmail.com>
2023-12-19 11:21:39 +0530

    12/19/2023-3

--- Fetch/Script.js
+++ Fetch/Script.js
@@ -1,32 +1,55 @@
 const BASE_URL = "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies";
 
-const dropdowns = document.querySelectorAll(".dropdown select");
-
+const  dropdowns = document.querySelectorAll(".dropdown select");
+const btn = document.querySelector("form button");
+const fromCurr = document.querySelector(".from select");
+const toCurr = document.querySelector(".to select");
+const msg = document.querySelector(".msg");
 let i = 0;
-
-for(let select of dropdowns){
+for (let select of dropdowns){
     for(code in countryList){
         let newOption = document.createElement("option");
-        newOption.innerText = code;
         newOption.value = code;
+        newOption.innerText = code;
         if(select.name === "from" && code === "USD"){
             newOption.selected = "selected";
         }else if(select.name === "to" && code === "INR"){
             newOption.selected = "selected";
-        }
-        select.append(newOption)
+        };
+        select.append(newOption);
     }
     select.addEventListener("change", (evt) => {
-        undateFlag(evt.target);
+        updateFlag(evt.target)
+
     })
 };
 
-
-const undateFlag = (element) => {
+const updateFlag = (element) => {
     let code = element.value;
-    let countreyCode = countryList[code];
-    let newSrc = `https://flagsapi.com/${countreyCode}/flat/64.png`;
+    let countryCode = countryList[code];
+    let newSrc = `https://flagsapi.com/${countryCode}/flat/64.png`;
     let img = element.parentElement.querySelector("img");
     img.src = newSrc;
 
+    
 };
+
+
+btn.addEventListener("click", async(evt) => {
+    evt.preventDefault();
+    let amount = document.querySelector(".amount input");
+    let value = amount.value;
+    if(value === "" || value < 1 ){
+        value = 1;
+        amount.value = "1";
+    }
+    // console.log(fromCurr,toCurr);
+    const URL = `${BASE_URL}/${fromCurr.value.toLowerCase()}/${toCurr.value.toLowerCase()}.json`;
+    let response = await fetch(URL);
+    let data = await response.json();
+    let rate = data[toCurr.value.toLowerCase()];
+   let finalAmount = value * rate;
+//    console.log(Math.floor(finalAmount))
+msg.innerText = `${value}${fromCurr.value} = ${Math.floor(finalAmount) + " "}${toCurr.value}`
+    
+});
