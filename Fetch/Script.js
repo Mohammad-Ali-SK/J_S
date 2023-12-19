cfda7fa4cdd36c0fb1871c1bec1199f1ab608a47
Mohammad Ali Sk <mohammadalisk050@gmail.com>
2023-12-19 20:37:11 +0530

    12/19/2023-4

--- Fetch/Script.js
+++ Fetch/Script.js
@@ -1,55 +1,54 @@
 const BASE_URL = "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies";
-
-const  dropdowns = document.querySelectorAll(".dropdown select");
-const btn = document.querySelector("form button");
+// console.log(BASE_URL
+const dropDown = document.querySelectorAll(".dropdown select");
+const btn = document.querySelector(".btn");
 const fromCurr = document.querySelector(".from select");
 const toCurr = document.querySelector(".to select");
 const msg = document.querySelector(".msg");
-let i = 0;
-for (let select of dropdowns){
-    for(code in countryList){
-        let newOption = document.createElement("option");
-        newOption.value = code;
-        newOption.innerText = code;
-        if(select.name === "from" && code === "USD"){
-            newOption.selected = "selected";
-        }else if(select.name === "to" && code === "INR"){
-            newOption.selected = "selected";
-        };
-        select.append(newOption);
-    }
-    select.addEventListener("change", (evt) => {
-        updateFlag(evt.target)
-
-    })
+// const img = document.querySelectorAll(".dropdown .img");
+for(let select of dropDown){
+   for(code in countryList){
+    let newOption = document.createElement("option");
+    newOption.value = code;
+    newOption.innerText = code;
+    
+    if(select.name === "from" && code === "USD"){
+        newOption.selected = "selected";
+    }else if(select.name === "to" && code ===  "INR"){
+        newOption.selected = "selected";
+    };
+    select.append(newOption);
+   }
+   select.addEventListener("change", (evt) => {
+    updateFlag(evt.target);
+   })
 };
 
-const updateFlag = (element) => {
+let updateFlag = (element) => {
     let code = element.value;
     let countryCode = countryList[code];
     let newSrc = `https://flagsapi.com/${countryCode}/flat/64.png`;
-    let img = element.parentElement.querySelector("img");
-    img.src = newSrc;
+   let img = element.parentElement.querySelector("img");
+   img.src = newSrc;
 
-    
 };
-
 
 btn.addEventListener("click", async(evt) => {
     evt.preventDefault();
     let amount = document.querySelector(".amount input");
-    let value = amount.value;
-    if(value === "" || value < 1 ){
-        value = 1;
+    let amtVal = amount.value;
+    if(amtVal === " " || amtVal < 0){
+        amtVal = 1;
         amount.value = "1";
-    }
-    // console.log(fromCurr,toCurr);
+    };
     const URL = `${BASE_URL}/${fromCurr.value.toLowerCase()}/${toCurr.value.toLowerCase()}.json`;
     let response = await fetch(URL);
     let data = await response.json();
     let rate = data[toCurr.value.toLowerCase()];
-   let finalAmount = value * rate;
-//    console.log(Math.floor(finalAmount))
-msg.innerText = `${value}${fromCurr.value} = ${Math.floor(finalAmount) + " "}${toCurr.value}`
+    let finalAmount = Number(amtVal * rate).toFixed(2);
+    msg.innerText = `${amtVal}${fromCurr.value} = ${finalAmount}${" " + toCurr.value}`
+    
+    
+     
     
 });
