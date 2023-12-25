030c59a0655d91421db918ea75cba2b02e7af394
Mohammad Ali Sk <mohammadalisk050@gmail.com>
2023-12-25 10:16:29 +0530

    12/25/23

---
+++ Practice_2/script.js
@@ -0,0 +1,10 @@
+window.addEventListener("mousemove", (e) => {
+    let box = document.querySelector(".box");
+let xval = gsap.utils.mapRange(0,window.innerWidth,100+box.getBoundingClientRect().width/2, window.innerWidth-(100+box.getBoundingClientRect().width/2),e.clientX)
+
+
+    gsap.to(".box", {
+        left: xval + "px",
+        ease:Power3,
+    })
+})
